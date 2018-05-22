cc9b652db3a49f3ca6b80824fa1985cae56ce4e4
ventaquil <konrad.golawski@gmail.com>
2018-05-22 11:18:14 +0200

    GH-4 Add statistics federate in new architecture

---
+++ src/mitsk/statistics/Federate.java
@@ -0,0 +1,101 @@
+package mitsk.statistics;
+
+import hla.rti1516e.InteractionClassHandle;
+import hla.rti1516e.ParameterHandle;
+import hla.rti1516e.RTIambassador;
+import mitsk.AbstractFederate;
+import mitsk.AbstractFederateAmbassador;
+import mitsk.statistics.object.Client;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class Federate extends AbstractFederate {
+    private static final int ITERATIONS = 20;
+
+    private InteractionClassHandle newInQueueInteractionClassHandle;
+
+    private ParameterHandle newInQueueInteractionClassClientIdParameterHandle;
+
+    private HashMap<Long, Client> clients = new HashMap<>();
+
+    public Federate(String federationName) throws Exception {
+        super(federationName);
+    }
+
+    public void addClientToStatistics(Long clientId) throws Exception {
+        Client client = new Client(getRTIAmbassador(), clientId, getFederateAmbassador().getFederateTime());
+
+        clients.put(clientId, client);
+    }
+
+    @Override
+    protected AbstractFederateAmbassador createAmbassador() {
+        return new Ambassador(this);
+    }
+
+    @Override
+    protected URL[] getFederationModules() throws MalformedURLException {
+        return new URL[]{
+            (new File("foms/Clients.xml")).toURI().toURL(),
+            (new File("foms/Queue.xml")).toURI().toURL(),
+            (new File("foms/Statistics.xml")).toURI().toURL()
+        };
+    }
+
+    @Override
+    protected URL[] getJoinModules() throws MalformedURLException {
+        return new URL[]{
+            (new File("foms/Statistics.xml")).toURI().toURL()
+        };
+    }
+
+    public InteractionClassHandle getNewInQueueInteractionClassHandle() {
+        return newInQueueInteractionClassHandle;
+    }
+
+    public ParameterHandle getNewInQueueInteractionClassClientIdParameterHandle() {
+        return newInQueueInteractionClassClientIdParameterHandle;
+    }
+
+    public static void main(String[] args) {
+        String federationName = args.length > 0 ? args[0] : "RestaurantFederation";
+
+        try {
+            new Federate(federationName).run();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void publish() throws Exception {
+        // empty
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            advanceTime(1.0);
+
+            log("Time Advanced to " + getFederateAmbassador().getFederateTime());
+        }
+
+        resignFederation();
+    }
+
+    @Override
+    protected void subscribe() throws Exception {
+        RTIambassador rtiAmbassador = getRTIAmbassador();
+
+        newInQueueInteractionClassHandle = rtiAmbassador.getInteractionClassHandle("HLAinteractionRoot.NewInQueue");
+
+        rtiAmbassador.subscribeInteractionClass(newInQueueInteractionClassHandle);
+
+        newInQueueInteractionClassClientIdParameterHandle = rtiAmbassador.getParameterHandle(newInQueueInteractionClassHandle, "clientId");
+    }
+}
