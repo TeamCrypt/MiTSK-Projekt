d8e5d41e33c86a400e271c1ec311bd495f9ec64b
malete <ma.turowska@gmail.com>
2018-06-05 10:32:12 +0200

    GH-8 Add Federate and Ambassador for Tables

---
+++ src/mitsk/tables/Federate.java
@@ -0,0 +1,117 @@
+package mitsk.tables;
+
+import hla.rti1516e.InteractionClassHandle;
+import hla.rti1516e.ParameterHandle;
+import hla.rti1516e.RTIambassador;
+import mitsk.AbstractFederate;
+import mitsk.AbstractFederateAmbassador;
+import mitsk.tables.object.Client;
+import mitsk.tables.object.Table;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Federate extends AbstractFederate {
+    private List<Table> tables;
+
+    private static final int ITERATIONS = 20;
+
+    private InteractionClassHandle clientTakesTableInteractionClassHandle;
+
+    private InteractionClassHandle clientLeavesTableInteractionClassHandle;
+
+    private ParameterHandle clientTakesTableInteractionClassClientIdParameterHandle;
+
+    private ParameterHandle clientTakesTableInteractionClassTableIdParameterHandle;
+
+    private ParameterHandle clientLeavesTableInteractionClassClientIdParameterHandle;
+
+    private ParameterHandle clientLeavesTableInteractionClassTableIdParameterHandle;
+
+
+    public Federate(String federationName, int size) throws Exception {
+        super(federationName);
+
+        if (size >= 1) {
+            tables = new ArrayList<>(size);
+        } else
+            tables = new ArrayList<>(3);
+    }
+
+    void clientTakesTable(Table table, Client client) {
+        table.setOccupied(client);
+    }
+
+    void clientLeavesTable(Table table) {
+        table.setFree();
+    }
+
+    @Override
+    protected AbstractFederateAmbassador createAmbassador() throws Exception {
+        return new Ambassador(this);
+    }
+
+    @Override
+    protected URL[] getFederationModules() throws MalformedURLException {
+        return new URL[]{
+                (new File("foms/Clients.xml")).toURI().toURL(),
+                (new File("foms/Kitchen.xml")).toURI().toURL(),
+                (new File("foms/Queue.xml")).toURI().toURL(),
+                (new File("foms/Statistics.xml")).toURI().toURL(),
+                (new File("foms/Tables.xml")).toURI().toURL()
+        };
+    }
+
+    @Override
+    protected URL[] getJoinModules() throws MalformedURLException {
+        return new URL[]{
+                (new File("foms/Tables.xml")).toURI().toURL()
+        };
+    }
+
+
+    public static void main(String[] args) {
+        String federationName = args.length > 0 ? args[0] : "RestaurantFederation";
+
+        try {
+            new Federate(federationName, 3).run();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void publish() throws Exception {
+        RTIambassador rtiAmbassador = getRTIAmbassador();
+
+        rtiAmbassador.subscribeInteractionClass(rtiAmbassador.getInteractionClassHandle("HLAinteractionRoot.ClientTakesTable"));
+
+        rtiAmbassador.subscribeInteractionClass(rtiAmbassador.getInteractionClassHandle("HLAinteractionRoot.ClientLeavesTable"));
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            sendInteraction();
+
+            advanceTime(1.0);
+
+            log("Time Advanced to " + getFederateAmbassador().getFederateTime());
+        }
+
+        resignFederation();
+    }
+
+    public void sendInteraction() {}
+
+    @Override
+    protected void subscribe() throws Exception {
+        RTIambassador rtiAmbassador = getRTIAmbassador();
+
+    }
+}
