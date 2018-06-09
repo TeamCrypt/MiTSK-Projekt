423f1e312ddfc797a095aa9f1eee36c04c655804
Tomasz Czechowski <Taumas@users.noreply.github.com>
2018-06-09 14:12:49 +0200

    GH-11 Add Federate and Ambassador for Waiters

---
+++ src/mitsk/waiters/Federate.java
@@ -0,0 +1,77 @@
+package mitsk.waiters;
+
+import hla.rti1516e.RTIambassador;
+import mitsk.AbstractFederate;
+import mitsk.AbstractFederateAmbassador;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Federate extends AbstractFederate {
+    private static final int ITERATIONS = 20;
+
+    public Federate(String federationName) throws Exception {
+        super(federationName);
+    }
+
+    @Override
+    protected AbstractFederateAmbassador createAmbassador() throws Exception {
+        return new Ambassador(this);
+    }
+
+    @Override
+    protected URL[] getFederationModules() throws MalformedURLException {
+        return new URL[]{
+            (new File("foms/Clients.xml")).toURI().toURL(),
+            (new File("foms/Waiters.xml")).toURI().toURL(),
+            (new File("foms/Kitchen.xml")).toURI().toURL(),
+            (new File("foms/Queue.xml")).toURI().toURL(),
+            (new File("foms/Statistics.xml")).toURI().toURL(),
+        };
+    }
+
+    @Override
+    protected URL[] getJoinModules() throws MalformedURLException {
+        return new URL[]{
+            (new File("foms/Waiters.xml")).toURI().toURL()
+        };
+    }
+
+    public static void main(String[] args) {
+        String federationName = args.length > 0 ? args[0] : "RestaurantFederation";
+
+        try {
+            new Federate(federationName).run();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void publish() throws Exception {
+        RTIambassador rtiAmbassador = getRTIAmbassador();
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run();
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            sendInteraction();
+
+            advanceTime(1.0);
+
+            log("Time Advanced to " + getFederateAmbassador().getFederateTime());
+        }
+
+        resignFederation();
+    }
+
+    public void sendInteraction() {}
+
+    @Override
+    protected void subscribe() throws Exception {
+        RTIambassador rtiAmbassador = getRTIAmbassador();
+    }
+}
